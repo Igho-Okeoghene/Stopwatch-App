af7713d8597c3927d1561520c1923384b6de51da
Okeoghene <okeogheneeunice@gmail.com>
2025-10-03 23:42:57 +0100

    added lap function

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 "use strict";
 
 const start = document
@@ -10,7 +9,10 @@ const stop = document
 const reset = document
   .getElementById("reset")
   .addEventListener("click", resetTimer);
+const lap = document.getElementById("lap").addEventListener("click", lapTimer);
 const timerDisplay = document.getElementById("timer");
+const lapDisplay = document.getElementById("lapDisplay");
+
 let hours = 0;
 let minutes = 0;
 let seconds = 0;
@@ -21,7 +23,7 @@ let buttonClicked = null;
  * Updates the timer display on the frontend.
  * Pads hours, minutes, and seconds with leading zeros.
  * Miliseconds are padded to 3 digits.
-**/
+ **/
 function updateDisplay() {
   timerDisplay.textContent = `${String(hours).padStart(2, "0")}:${String(
     minutes
@@ -35,7 +37,7 @@ function updateDisplay() {
  * Starts the stopwatch timer.
  * Prevents multiple intervals from being set.
  * Increments miliseconds, seconds, minutes, and hours accordingly.
-**/
+ **/
 function startTimer() {
   if (buttonClicked) return;
   buttonClicked = setInterval(() => {
@@ -59,20 +61,33 @@ function startTimer() {
 /**
  * Stops the stopwatch timer.
  * Clears the interval if running.
-**/
+ **/
 function stopTimer() {
   clearInterval(buttonClicked);
   buttonClicked = null;
 }
 
 /**
+ * calls the stopwatch timer function.
  * Resets the stopwatch timer to zero.
  * Updates the display to show all zeros.
-**/
+ **/
 function resetTimer() {
+  stopTimer();
   miliseconds = 0;
   seconds = 0;
   minutes = 0;
   hours = 0;
   updateDisplay();
+}
+
+/**
+ * displays the current time without pausing or stopping the stopwatch timer function.
+ * Updates the values on display to the lap array
+ **/
+function lapTimer() {
+  let lapArr = [];
+  lapArr.push(timerDisplay.textContent);
+  console.log(lapArr);
+  lapDisplay.textContent = `Current Lap Time: ${lapArr}`;
 }
